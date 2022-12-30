df8c6fc7090824c9b2b16a7d12750ada2f03efed
SylasB <sylasboniek@gmail.com>
2022-12-30 16:50:24 -0700

    Made all the data points work based on any location put in.

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 window.onload = getWeather();
 function getWeather() {
+const location = document.getElementById('locationInput').value;
 const currentTemp = document.getElementById('currentTemp');
+const feelsLike = document.getElementById('feelsLike');
 const windSpeed = document.getElementById('currentWind');
 const humidity = document.getElementById('currentHumidity');
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Denver&APPID=35840eb6941e2c32e1c9644356825fa4', {mode: 'cors'})
+const currentLocation = document.getElementById('searchLocation');
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=35840eb6941e2c32e1c9644356825fa4`, {mode: 'cors'})
     .then(function(response) {
       return response.json();
       
     })
     .then(function(response) {
+        temperature = Math.ceil((response.main.temp - 273.15) *1.8) + 32;
+        feelsLikeTemp = Math.ceil((response.main.feels_like - 273.15) *1.8) + 32;
         console.log(response);
-        console.log(response.main.temp);
-            currentTemp.textContent = `Current Temperature: ${response.main.temp} Degrees `
 
-        console.log(response.wind.speed);
+            currentTemp.textContent = `${temperature}\u00B0F`
+
+        console.log(response.main.feels_like);    
+            feelsLike.textContent = `Feels Like: ${feelsLikeTemp}\u00B0F`
+
             windSpeed.textContent = `Wind Speed: ${response.wind.speed} MPH`;
 
-        console.log(response.main.humidity);
+
             humidity.textContent = `Humidity: ${response.main.humidity}%`;
+
+
+            currentLocation.textContent = `${response.name}`;
     });
 }
