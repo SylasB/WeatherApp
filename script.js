ba10bc85e309eb49042054437148203734b44e21
SylasB <sylasboniek@gmail.com>
2022-12-30 17:21:03 -0700

    Added the final styling and error catch.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const feelsLike = document.getElementById('feelsLike');
 const windSpeed = document.getElementById('currentWind');
 const humidity = document.getElementById('currentHumidity');
 const currentLocation = document.getElementById('searchLocation');
+const errorMsg = document.getElementById('errorMsg');
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=35840eb6941e2c32e1c9644356825fa4`, {mode: 'cors'})
     .then(function(response) {
       return response.json();
@@ -14,11 +15,9 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=35840
     .then(function(response) {
         temperature = Math.ceil((response.main.temp - 273.15) *1.8) + 32;
         feelsLikeTemp = Math.ceil((response.main.feels_like - 273.15) *1.8) + 32;
-        console.log(response);
 
             currentTemp.textContent = `${temperature}\u00B0F`
-
-        console.log(response.main.feels_like);    
+  
             feelsLike.textContent = `Feels Like: ${feelsLikeTemp}\u00B0F`
 
             windSpeed.textContent = `Wind Speed: ${response.wind.speed} MPH`;
@@ -28,5 +27,13 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=35840
 
 
             currentLocation.textContent = `${response.name}`;
+        
+        errorMsg.className = 'hidden';
+        errorMsg.textContent = '';
+    })
+    .catch(function() {
+        errorMsg.className = 'activeError';
+        errorMsg.textContent = 'Please Enter A City'
+        console.log('ERROR');
     });
 }
